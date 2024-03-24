0d95f55ece88b1bbdac1e6e68c9f316cad80c655
ravitejamanchala <manchala0024@gmail.com>
2024-03-25 02:19:12 +0530

    logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import logo from "./logo.png";
 const Navbar = () => {
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 py-2">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 py-6">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/logo.png"  className="h-20" alt="RJ Forms Logo" />
+                    <img src={logo} className="h-10" alt="RJ Forms Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Forms</span>
                 </a>
                 <div className="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
